0e76e5524326f77957b87410eb7a18bdc61be61a
saimaghani <saimaghani31@gmail.com>
2025-07-30 22:38:25 +0500

    second commit

---
+++ test2.py
@@ -0,0 +1,9 @@
+print("""Enumerating objects: 4, done.
+Counting objects: 100% (4/4), done.
+Delta compression using up to 4 threads
+Compressing objects: 100% (3/3), done.
+Writing objects: 100% (4/4), 354 bytes | 32.00 KiB/s, done.
+Total 4 (delta 0), reused 0 (delta 0), pack-reused 0 (from 0)        
+To https://github.com/saimaghani/python_class.git
+ * [new branch]      main -> main
+branch 'main' set up to track 'origin/main'.""")
